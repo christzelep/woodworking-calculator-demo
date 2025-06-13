0fa822e39de43278cfbdc443c43cdff5fc0b995e
christzelep <trendinggadgets23@gmail.com>
2025-06-13 18:58:37 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,46 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'woodworking-assistant-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/blue_circle.svg',
+  '/green_square.svg',
+  '/manifest.json'
+];
+
+// Install service worker and cache assets
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll(['/', '/index.html']);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// Serve cached content when offline
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
